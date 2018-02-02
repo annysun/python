a56214908a74e6ed6ccece6b48630bc28c2ede69
Wang <anny.wang@sap.com>
2018-02-02 17:05:15 +0800

    array-circulate

---
+++ c.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+print(len("adsbdf"))
+anny=[520,1314,999,8888888]
+anny.append(100)
+anny.insert(3,666)
+anny.pop()
+anny.pop(1)
+anny[0]=[520,1314]
+print(anny)
+b=(1,2,3)
+print(b)
+k=[1,2,3,4,5,6,7,8]
+for a in k:
+    print(a)
+r=range(101)
+sum=0
+for b in r:
+    sum=sum+b
+print(sum)
